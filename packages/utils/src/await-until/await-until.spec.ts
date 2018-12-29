5582a2272e6c053e0eb0b3796ca0739969c9b50b
AlexMin314 <m98stone@gmail.com>
2018-12-29 20:57:54 +0800

    await util

---
+++ packages/utils/src/await-until/await-until.spec.ts
@@ -0,0 +1,18 @@
+import { until } from './await-until';
+
+describe('await until', () => {
+  test('should return a value when promise resolved', async () => {
+    const [err, data] = await until<{ data: number }>(
+      Promise.resolve({ data: 1 }),
+    );
+    expect(err).toBeUndefined();
+    expect(data).toEqual({ data: 1 });
+  });
+  test('should return an error when promise rejected', async () => {
+    const [err, data] = await until<{ error: string }>(
+      Promise.reject({ error: 'error here' }),
+    );
+    expect(err).toEqual({ error: 'error here' });
+    expect(data).toBeUndefined();
+  });
+});
